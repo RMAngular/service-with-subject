1c973cc06ed788c1e3b421733b83684b9cbb9927
Jesse Sanders <jesse.sanders@briebug.com>
2018-09-10 16:48:43 -0600

    added html wireups

--- src/app/dashboard/components/top-nav/top-nav.component.ts
+++ src/app/dashboard/components/top-nav/top-nav.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-top-nav',
-  templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.css']
+  selector: "app-top-nav",
+  templateUrl: "./top-nav.component.html",
+  styleUrls: ["./top-nav.component.css"]
 })
 export class TopNavComponent implements OnInit {
+  role: string = "Standard";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRoleChange() {
+    console.log(this.role);
   }
-
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
 import { DashboardComponent } from "./containers/dashboard/dashboard.component";
 import { UserListComponent } from "./components/user-list/user-list.component";
 import { TopNavComponent } from "./components/top-nav/top-nav.component";
 import { PermissionsComponent } from "./components/permissions/permissions.component";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [
     DashboardComponent,
     UserListComponent,
